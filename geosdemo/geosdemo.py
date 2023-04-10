1b18b0a4eb672d8907e7b628036e6ea8d7e5c650
Felipe Fonseca <felipe.fonsecaa14@gmail.com>
2023-04-10 16:35:29 +0200

    First changes to geosdemo package

--- geosdemo/geosdemo.py
+++ geosdemo/geosdemo.py
@@ -1,1 +1,19 @@
 """Main module."""
+
+import string 
+import random
+
+#Created on youtube lesson week 10
+def generate_random_string(length, upper= False, digits = False, punctuation= False):
+    
+    letters = string.ascii_lowercase
+    if upper:
+         letters += string.ascii_uppercase
+    if digits:
+         letters += string.digits
+    if punctuation:
+         letters += string.pun
+        
+    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
