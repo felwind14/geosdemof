03a21bc89fd762124ca76cc3acd142329cd7a586
Felipe Fonseca <felipe.fonsecaa14@gmail.com>
2023-04-11 17:49:01 +0200

    Adding draw control functionality

--- geosdemo/geosdemo.py
+++ geosdemo/geosdemo.py
@@ -29,7 +29,51 @@ class Map (ipyleaflet.Map):   #we are going to build based on this
         print(f"this is what the user is providing:{kwargs}") 
         self.add_control(search_control)   
 
-    
+    def add_draw_control(self, **kwargs):  #based on control  example
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """  
+        draw_control = ipyleaflet.DrawControl(**kwargs) 
+
+        #customizing the draw control
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }  
+        }    
+       
+        
+        self.add_control(draw_control)
 
 
 #Created on youtube lesson week 10
